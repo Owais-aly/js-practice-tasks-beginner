1ac1d4bbc35425ff3f2ddf97f59b8204faf1c5fb
Owais ali <85180794+Owais-aly@users.noreply.github.com>
2021-06-06 13:13:36 +0500

    Add files via upload

---
+++ practice1.js
@@ -0,0 +1,9 @@
+//alert("Welcome");
+
+//alert("Error! Please enter a valid password.");
+
+//alert("welcome to js land...\nHappy Coding");
+
+//alert("Welcome to JS Land...");
+//alert("Happy Coding!");
+
